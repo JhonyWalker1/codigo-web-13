003a4f44a98901fdf856868d8b3b5db1b5b315f7
JhonyWalker1 <jhonatanaliaga4@gmail.com>
2022-03-13 20:53:16 -0500

    avance proyecto

---
+++ semana7/dia4/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+// para poder crear nuestras rutas
+//para este ejemplo vamos a crar un archivo llamado movies.js
+// donde guardaremos un array de objetos
